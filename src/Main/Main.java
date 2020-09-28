3c5cd033a0135ae0d10e04761c9af00801c0bfe2
bobo6668 <boboxie966668@gmail.com>
2020-09-28 22:42:57 +0800

    用IDEA新建了第一个Java项目

---
+++ src/Main/Main.java
@@ -0,0 +1,25 @@
+package Main;
+
+//import java.util.*;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("hello world");
+        String str = new String();
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> map = new HashMap<>();
+    }
+
+}
+
+class Node {
+
+}
+
+
