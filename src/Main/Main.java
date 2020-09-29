ee163a5852ed1b0015aae7124807ee5052a32324
bobo6668 <boboxie966668@gmail.com>
2020-09-29 23:00:21 +0800

    先整理了一波Java入门的笔记，不然也不好写代码。面向过程的快速整理完了。面向对象todo

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,25 +1,23 @@
 package Main;
 
-//import java.util.*;
-
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello world");
-        String str = new String();
         Scanner scanner = new Scanner(System.in);
-        Map<String, String> map = new HashMap<>();
+//        int p1Num = scanner.nextInt();
+//
+//        for(int i=0; i<p1Num; i++){
+//        }
+//
+//        System.out.println("hello world");
     }
 
 }
 
-class Node {
-
-}
+//class PolyNode {
+//
+//}
 
 
